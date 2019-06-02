53baa379eace0fbdcd6ab49136de37e4814abe8f
Hugo <hugodeiro@gmail.com>
2019-06-02 13:52:47 -0300

    ngIf

---
+++ directives/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DirectiveNgifComponent } from './directive-ngif/directive-ngif.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DirectiveNgifComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ directives/src/app/directive-ngif/directive-ngif.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-ngif',
+  templateUrl: './directive-ngif.component.html',
+  styleUrls: ['./directive-ngif.component.scss']
+})
+export class DirectiveNgifComponent implements OnInit {
+
+  classes: string[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
