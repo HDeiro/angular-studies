887fbf63e16c1b83ed2fe4474fb0dcdda0f82b48
Hugo <hugodeiro@gmail.com>
2019-06-02 18:46:50 -0300

    ngClass

---
+++ directives/src/app/directive-ngclass/directive-ngclass.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive-ngclass',
+  templateUrl: './directive-ngclass.component.html',
+  styleUrls: ['./directive-ngclass.component.scss']
+})
+export class DirectiveNgclassComponent implements OnInit {
+
+  active: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
